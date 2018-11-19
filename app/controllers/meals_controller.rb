5576ab327476ed36849677a2058c7a0ee10bf007
Narmeen Massalha <narmeenmassalha4975@gmail.com>
2018-11-19 11:46:48 +0200

    created meal model and controller

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,31 @@
+class MealsController < ApplicationController
+
+	def new 
+		@meal = Meal.new
+	end
+	
+	def show
+	   @meal = Meal.find(params[:id])
+	end
+
+	def create
+		 @meal = Meal.new(meal_params)
+      if @meal.save
+      	redirect_to meal_path(@meal)
+      else 
+       render "new"
+      end 
+
+	end
+
+	private 
+
+	def set_params
+		@meal = Meal.find(params[:id])
+		
+    end
+
+    def meal_params
+    	params.require(:meal).permit(:name,:date,:location,:type_of_food,:price,:capacity)
+    end	
+end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+  	@meal = Meal.all
   end
 end
