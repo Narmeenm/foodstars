131ee3d6756533a7fb22b7e67a630c6c928b0f70
zaneta uba <zaneta.uba@gmail.com>
2018-11-19 14:40:47 +0200

    added booking model and controller

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,9 +1,10 @@
 class MealsController < ApplicationController
 
-	def new 
+	def new
 		@meal = Meal.new
+
 	end
-	
+
 	def show
 	   @meal = Meal.find(params[:id])
 	end
@@ -12,20 +13,20 @@ class MealsController < ApplicationController
 		 @meal = Meal.new(meal_params)
       if @meal.save
       	redirect_to meal_path(@meal)
-      else 
+      else
        render "new"
-      end 
+      end
 
 	end
 
-	private 
+	private
 
 	def set_params
 		@meal = Meal.find(params[:id])
-		
+
     end
 
     def meal_params
     	params.require(:meal).permit(:name,:date,:location,:type_of_food,:price,:capacity)
-    end	
+    end
 end
