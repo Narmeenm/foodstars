1b64a82bc3acacf3e7e0280b648a33423ce6bb60
zaneta uba <zaneta.uba@gmail.com>
2018-11-20 22:23:13 +0200

    map added to meals/ show

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,6 @@
 class Meal < ApplicationRecord
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
   belongs_to :host, foreign_key: :user_id, class_name: 'User'
   has_many :bookings, dependent: :destroy
   has_many :guests, through: :bookings, source: :user, dependent: :destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_101252) do
+ActiveRecord::Schema.define(version: 2018_11_20_193332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2018_11_19_101252) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_meals_on_user_id"
   end
 
